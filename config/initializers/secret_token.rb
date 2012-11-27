c885a8473c74198123d0e9981e1717ea88861913
Ishchenko <ishchenko@Notebook-PC.(none)>
2012-11-27 22:00:08 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kntu::Application.config.secret_token = '44f8c9cc88e10e7951f069ce560f862708f1422814032b578bedcfde1055016160e101450bf77e53fe7feda729627db7e89af2968ba5c2896d3aaf6d78e3624a'
