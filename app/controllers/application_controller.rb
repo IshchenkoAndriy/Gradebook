e12464f62aee1792211b3a58b52737740172aae9
Andrew Ishchenko <ishchenkoandriy@ukr.net>
2013-01-07 21:42:37 +0200

    Added localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+  
+  # def after_sign_in_path_for(resource_or_scope)
+# 
+    # if resource_or_scope.is_a?(User) && resource_or_scope.locale !=  I18n.locale
+      # I18n.locale = resource_or_scope.locale
+    # end
+#   
+    # super
+  # end
+  
+  private
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+  
+  def set_locale
+    logger.debug "Params locale #{params[:locale]}"
+    I18n.locale = params[:locale] || I18n.default_locale
+    logger.debug "Seted locale #{I18n.locale}"
   end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,5 @@ class MainController < ApplicationController
   def index
     @articles = Article.find(:all, :limit => 3, :order=> 'created_at desc');
   end
+  
 end
